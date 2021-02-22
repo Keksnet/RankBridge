2cb2277ad38a2cbcc729a2e68ba0245f739a0988
Keksnet <56829999+Keksnet@users.noreply.github.com>
2021-02-22 10:14:27 +0100

    Bugfix.

--- src/de/neo/rankbridge/teamspeak/listener/MessageSendListener.java
+++ src/de/neo/rankbridge/teamspeak/listener/MessageSendListener.java
@@ -52,7 +52,7 @@ public class MessageSendListener implements BridgeEventListener {
 					for(int i = 0; i < cs.size(); i++) {
 						Client c = cs.get(i);
 						if(c.getIp().equals(ip) && c.isRegularClient()) {
-							css[i] = c;
+							css[clients] = c;
 							clients++;
 						}
 					}
